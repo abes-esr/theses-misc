e8614e7f233e57be684aab55599487177b198105
Clément Delafontaine <clment.delafontaine@gmail.com>
2026-04-27 16:39:39 +0200

    SOA-567 fix: inversion processors

--- src/main/java/fr/abes/theses/thesesmisc/tasklets/diviserVedetteRameau/DiviserVedetteRameauProcessor.java
+++ src/main/java/fr/abes/theses/thesesmisc/tasklets/diviserVedetteRameau/DiviserVedetteRameauProcessor.java
@@ -2,24 +2,39 @@ package fr.abes.theses.thesesmisc.tasklets.diviserVedetteRameau;
 
 import fr.abes.theses.thesesmisc.model.DocumentProcess;
 import fr.abes.theses.thesesmisc.model.Tef;
+import fr.abes.theses.thesesmisc.utils.ScissionRameauEntry;
+import fr.abes.theses.thesesmisc.utils.ScissionRameauList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
-    @Slf4j
-    @Component
-    public class DiviserVedetteRameauProcessor implements ItemProcessor<DocumentProcess, DocumentProcess> {
-        @Override
-        public DocumentProcess process(DocumentProcess documentProcess) throws Exception {
+import java.util.List;
 
-            try {
-                Tef documentTef = new Tef(documentProcess.document.getDoc());
-                documentProcess.edited = documentTef.deleteSubdivisionDeForme();
-                documentProcess.document.setDoc(documentTef.documentTef.asXML());
-            } catch (Exception e){
-                log.info("Error in processor, doc : " + documentProcess.document.getIdDoc());
+/**
+ * Prend un tef et vérifie pour chaque ligne de ScissionRameauList si le sujet Rameau concerné par une scission est présent
+ * Et le remplace par les deux sujets de sa subdivision
+ */
+@Slf4j
+@Component
+public class DiviserVedetteRameauProcessor implements ItemProcessor<DocumentProcess, DocumentProcess> {
+    @Override
+    public DocumentProcess process(DocumentProcess documentProcess) throws Exception {
+        // Récupérer la liste des scissions rameau
+        List<ScissionRameauEntry> scissionRameauEntries = ScissionRameauList.getEntries();
+        Tef documentTef = new Tef(documentProcess.document.getDoc());
+        try {
+            for (ScissionRameauEntry entry : scissionRameauEntries) {
+                documentProcess.edited = documentProcess.edited || documentTef.searchAndReplaceScissionRameau(entry); // edited reste à true meme si les prochaines entrées n'apparaissent pas dans le tef
             }
-
-            return documentProcess;
+        } catch (Exception e) {
+            log.info("Error in SubdivisionDeFormeProcessor, doc : " + documentProcess.document.getIdDoc());
         }
+
+        if (documentProcess.edited) {
+            log.debug("Document TEF modifié: {}", documentProcess.document.getIdDoc());
+        }
+        documentProcess.document.setDoc(documentTef.documentTef.asXML());
+
+        return documentProcess;
     }
+}
